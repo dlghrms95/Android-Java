605d898e98d23686d689dce58f509bdc478ccec3
HoKeun.LEE <dlghrms95@gmail.com>
2021-06-07 19:57:32 +0900

    Update K번째 수 (프로그래머스).kt

--- PS/K번째 수 (프로그래머스).kt
+++ PS/K번째 수 (프로그래머스).kt
@@ -2,6 +2,7 @@ class Solution {
     fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
         val answer = IntArray(commands.size)
         val list: ArrayList<Int> = ArrayList()
+        
         for (i in commands.indices) {
             for (j in commands[i][0] until commands[i][1] + 1) {
                 list.add(array[j - 1])
