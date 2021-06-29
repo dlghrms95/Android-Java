d7f21d83e9fd806e85ed992874db6796f5a18dae
HoKeun.LEE <dlghrms95@gmail.com>
2021-06-29 22:25:24 +0900

    2021-06-29

--- PS/튜플 (2019 카카오 인턴쉽).kt
+++ PS/튜플 (2019 카카오 인턴쉽).kt
@@ -38,3 +38,17 @@ class Solution {
         return answer.toIntArray()
     }
 }
+
+// 다른사람 풀이 1
+class Solution {
+    fun solution(s: String): IntArray {
+        return s.substring(2 until s.length-2)
+            .split("},{")
+            .asSequence()
+            .map { it.split(",").map { num -> num.toInt() } }
+            .toList()
+            .sortedBy { it.size }
+            .fold(setOf<Int>()) { acc, list -> acc.union(list) }
+            .toIntArray()
+    }
+}
